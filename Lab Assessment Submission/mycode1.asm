a0715efb9728b52b51bafa89d332ed75f0f9f7c2
Rupak95 <52215838+Rupak95@users.noreply.github.com>
2020-11-17 22:39:06 +0600

    File added

---
+++ Lab Assessment Submission/mycode1.asm
@@ -0,0 +1,16 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ax, 1
+add ax, 5
+add ax, 5
+
+
+ret
+
+
+
+
